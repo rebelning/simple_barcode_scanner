e290470ede5b80e72afcac31fbb9fe43d0cfe114
jdy876884206 <876884206@qq.com>
2024-05-09 14:27:04 +0800

    修改扫码样式

--- lib/screens/web.dart
+++ lib/screens/web.dart
@@ -55,16 +55,16 @@ class BarcodeScanner extends StatelessWidget {
     final width = MediaQuery.of(context).size.width;
     final height = width / (16 / 9);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(appBarTitle ?? kScanPageTitle),
-        centerTitle: centerTitle,
-      ),
+      // appBar: AppBar(
+      //   title: Text(appBarTitle ?? kScanPageTitle),
+      //   centerTitle: centerTitle,
+      // ),
       body: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(
-              height: height,
-              width: width,
+              height: 600,
+              width: 600,
               child: HtmlElementView(
                 viewType: createdViewId,
               ),
